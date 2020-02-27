bc329c54730a08373b53dd0c1bad5e20d47e83a1
Anastasiia Loginova <barbisize@gmail.com>
2020-02-27 20:30:24 +0300

    Внесены изменения в app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+
 app.get("*", (req, res) => {
   res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
 });
